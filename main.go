dfd30be7cb16f1e7e11cb20f490e335fa1631344
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-03-06 21:41:40 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+// main.go
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"ecommerce-api/database"
+	"ecommerce-api/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	database.InitDB() // Inisialisasi database
+
+	r := mux.NewRouter()
+	routes.RegisterProductRoutes(r)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", r)
+}
